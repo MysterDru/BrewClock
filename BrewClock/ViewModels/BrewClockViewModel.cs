2b7c8889e111178d5b9384a088f1e466b8d75dfc
Drew Frisk <dfrisk@bhtp.com>
2015-04-26 14:57:00 -0500

    made things work for iOS properly
    removed XLabs, updated Forms package

--- BrewClock/ViewModels/BrewClockViewModel.cs
+++ BrewClock/ViewModels/BrewClockViewModel.cs
@@ -2,158 +2,247 @@
 using PropertyChanged;
 using System.Windows.Input;
 using Xamarin.Forms;
-using XLabs;
-#if __IOS__
-using Foundation;
-#endif
-
 #if __ANDROID__
 using Android.OS;
+#elif __IOS__
+using Foundation;
 #endif
 
 namespace BrewClock
 {
-	[ImplementPropertyChanged]
-	public class BrewClockViewModel
-	{
-		private ICommand startBrewCommand;
-		private ICommand addBrewTimeCommand;
-		private ICommand decreaseBrewTimeCommand;
-		private int brewTime;
-		private BrewClockTimer brewCountTimer;
+    [ImplementPropertyChanged]
+    public class BrewClockViewModel
+    {
+        private ICommand changeBrewingStateCommand;
+        private IBrewClockTimer brewCountTimer;
 
-		public string Title { get; private set; } = "Brew Clock";
+        /// <summary>
+        /// Identifies the title of the page
+        /// </summary>
+        public string Title { get; private set; }
 
-		public int BrewTime { get; set; } = 3;
+        /// <summary>
+        /// Identifies the curent selected brew time in minutes
+        /// </summary>
+        public int BrewTime { get; set; }
 
-		public long BrewCountDown { get; private set; } = 0;
+        /// <summary>
+        /// Value displayed to the user based on the remaining brewtime
+        /// </summary>
+        public string BrewTimeDisplay
+        {
+            get
+            {
+                if (IsBrewing)
+                {
+                    return string.Format("{0}s", BrewCountDown);
+                }
+                else if (BrewTime == 0 && !IsBrewing)
+                {
+                    return "Brew Up!";
+                }
+                else
+                {
+                    return string.Format("{0}m", BrewTime);
+                }
+            }
+        }
 
-		public int BrewCount { get; set; } = 0;
+        /// <summary>
+        /// Identifies the seconds remaining before the brew is completed
+        /// </summary>
+        public long BrewCountDown { get; private set; }
 
-		public int MinimumBrewCount { get; private set; } = 1;
+        /// <summary>
+        /// Total number of brews the user has made
+        /// </summary>
+        public int BrewCount { get; set; }
 
-		public bool IsBrewing { get; set; } = false;
+        /// <summary>
+        /// Identifies the minimum time allowed for brewing
+        /// </summary>
+        public int MinimumBrewTime { get; private set; }
 
-		public string BrewTimeDisplay {
-			get {
-				if (IsBrewing) {
-					return string.Format ("{0}s", BrewCountDown);
-				}
-				else if(BrewTime == 0 && !IsBrewing) {
-					return "Brew Up!";
-				}
-				else {
-					return string.Format ("{0}m", BrewTime);
-				} 
-			}
-		}
+        /// <summary>
+        /// Identifies the maximum time allowed for brewing
+        /// </summary>
+        public int MaximumBrewTime { get; private set; }
 
-		public string StartDisplay { get; set; } = "Start";
+        /// <summary>
+        /// Identifies if the brew timer is running
+        /// </summary>
+        public bool IsBrewing { get; set; }
 
-		public ICommand StartBrewCommand {
-			get 
-			{
-				return startBrewCommand ?? (startBrewCommand = new Command (() => {
-					if (IsBrewing)
-						StopBrewing ();
-					else
-						StartBrewing ();
-				}));
-			}
-		}
+        /// <summary>
+        /// Identifies the text value that should be displayed in the button text.
+        /// </summary>
+        public string StartDisplay { get; set; }
 
-//		public ICommand AddBrewTimeCommand {
-//			get {
-//				return addBrewTimeCommand ?? (addBrewTimeCommand = new Command (() => {
-//					BrewTime += 1;
-//				}));
-//			}
-//		}
-//
-//		public ICommand DecreaseBrewTimeCommand {
-//			get {
-//				return decreaseBrewTimeCommand?? (decreaseBrewTimeCommand = new Command(() => {
-//					BrewTime -= 1;
-//				}));
-//			}
-//		}
+        /// <summary>
+        /// The command that should be executed in order to start or stop the brew timer
+        /// </summary>
+        public ICommand ChangeBrewingStateCommand
+        {
+            get
+            {
+                return changeBrewingStateCommand ?? (changeBrewingStateCommand = new Command(() =>
+                {
+                    if (this.IsBrewing)
+                    {
+                        this.StopBrewing();
+                    }
+                    else
+                    {
+                        this.StartBrewing();
+                    }
+                }));
+            }
+        }
 
+        public BrewClockViewModel()
+        {
+            this.Title = "Brew Clock";
+            this.BrewTime = 3;
+            this.BrewCountDown = 0;
+            this.MinimumBrewTime = 1;
+            this.MaximumBrewTime = 10;
+            this.IsBrewing = false;
+            this.StartDisplay = "Start";
+        }
 
-		public BrewClockViewModel ()
-		{
-		}
+        private void StartBrewing()
+        {
+            var secs = BrewTime * 60 * 1000;
 
-		private void StartBrewing()
-		{
-			var secs = BrewTime * 60 * 1000;
+#if __ANDROID__
+            this.brewCountTimer = new BrewClockTimer(secs, 1000);
+#elif __IOS__
+            this.brewCountTimer = new BrewClockTimer(secs, 1000);
+#endif
+            this.BrewCountDown = secs / 1000;
 
-			brewCountTimer = new BrewClockTimer (secs, 1000);
-			BrewCountDown = secs / 1000;
+            this.brewCountTimer.TickChanged += ((object sender, EventArgs<long> e) =>
+            {
+                BrewCountDown = (e.Value / 1000);
+            });
 
-			brewCountTimer.TickChanged	= ((object sender, EventArgs<long> e) => {
-				BrewCountDown = (e.Value / 1000);
-//				BrewTimeDisplay = String.Format("{0}s", e.Value / 1000);
-			});
+            this.brewCountTimer.Finished += ((object sender, EventArgs e) =>
+            {
+                IsBrewing = false;
+                BrewCount += 1;
+                StartDisplay = "Start";
+            });
 
-			brewCountTimer.Finished = ((object sender, EventArgs e) => {
-				IsBrewing = false;
-				BrewCount += 1;
-				StartDisplay = "Start";
-			});
+            this.brewCountTimer.StartTimer();
+            this.StartDisplay = "Stop";
 
-			brewCountTimer.Start ();
-			StartDisplay = "Stop";
+            this.IsBrewing = true;
+        }
 
-			IsBrewing = true;
-		}
+        private void StopBrewing()
+        {
+            if (this.brewCountTimer != null)
+            {
+                this.brewCountTimer.CancelTimer();
+            }
 
-		private void StopBrewing()
-		{
-			if (brewCountTimer != null) {
-				brewCountTimer.Cancel ();
-			}
+            this.IsBrewing = false;
+            this.StartDisplay = "Start";
+        }
 
-			IsBrewing = false;
-			StartDisplay = "Start";
-		}
+        /// <summary>
+        /// Interface for abstracting timer logic to each platform
+        /// </summary>
+        private interface IBrewClockTimer
+        {
+            event EventHandler<EventArgs<long>> TickChanged;
+            event EventHandler Finished;
 
-		#if __ANDROID__
-		private class BrewClockTimer : CountDownTimer
-		{
-			public EventHandler Finished = delegate { };
+            void CancelTimer();
+            void StartTimer();
+        }
 
-			public EventHandler<EventArgs<long>> TickChanged = delegate { };
+#if __ANDROID__
+        private class BrewClockTimer : CountDownTimer, IBrewClockTimer
+        {
+            public event EventHandler Finished = delegate { };
 
-			public BrewClockTimer (long millisInFuture, long countDownInterval) : base(millisInFuture, countDownInterval) {
-			}
+            public event EventHandler<EventArgs<long>> TickChanged = delegate { };
 
-			public override void OnTick (long millisUntilFinished)
-			{
-				TickChanged(this, new EventArgs<long>(millisUntilFinished));
-			}
+            public BrewClockTimer(long millisInFuture, long countDownInterval)
+                : base(millisInFuture, countDownInterval)
+            {
+            }
 
-			public override void OnFinish ()
-			{
-				Finished (this, new EventArgs ());
-			}
-		}
-		#endif
+            public override void OnTick(long millisUntilFinished)
+            {
+                this.TickChanged(this, new EventArgs<long>(millisUntilFinished));
+            }
 
-		#if _IOS_
-		private class BrewClockTimer {
+            public override void OnFinish()
+            {
+                this.Finished(this, new EventArgs());
+            }
 
-			public EventHandler Finished = delegate { };
+            public void CancelTimer()
+            {
+                this.Cancel();
+            }
 
-			public EventHandler<EventArgs<long>> TickChanged = delegate { };
+            public void StartTimer()
+            {
+                this.Start();
+            }
+        }
+#elif __IOS__
+        private class BrewClockTimer : IBrewClockTimer
+        {
+            public event EventHandler Finished = delegate { };
 
-			public BrewClockTimer(long millisInFuture, long countDownInterval)
-			{
+            public event EventHandler<EventArgs<long>> TickChanged = delegate { };
 
-				var timer =	NSTimer.CreateScheduledTimer(millisInFuture / 1000, HandleAction);
+            private NSTimer timer;
+            private long millisInFuture, countDownInterval, hours, minutes, seconds, secondsLeft;
 
-			}
+            public BrewClockTimer(long millisInFuture, long countDownInterval)
+            {
+                this.millisInFuture = millisInFuture;
+                this.countDownInterval = countDownInterval;
+            }
 
-		}
-		#endif
-	}
+            public void CancelTimer()
+            {
+                this.timer.Invalidate();
+            }
+
+            public void StartTimer()
+            {
+                this.secondsLeft = this.millisInFuture / 1000;
+
+                this.timer = NSTimer.CreateRepeatingScheduledTimer(this.countDownInterval / 1000, this.UpdateCounter);
+            }
+
+            private void UpdateCounter(NSTimer theTimer)
+            {
+                if (this.secondsLeft > 0)
+                {
+                    this.secondsLeft--;
+                    this.hours = secondsLeft / 3600;
+                    this.minutes = (secondsLeft % 3600) / 60;
+                    this.seconds = (secondsLeft % 3600) % 60;
+
+                    // expexted value is in milliseconds
+                    this.TickChanged(this, new EventArgs<long>(this.secondsLeft * 1000));
+                }
+                else
+                {
+                    this.Finished(this, new EventArgs());
+                    this.CancelTimer();
+
+                    this.secondsLeft = this.millisInFuture / 1000;
+                }
+            }
+        }
+#endif
+    }
 }
